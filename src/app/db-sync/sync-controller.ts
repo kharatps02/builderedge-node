1afa9e71e7df8694c01a712095738a20bc169c3a
rk-7 <30998401+rk-7@users.noreply.github.com>
2018-02-21 19:52:29 -0800

    Added Recursive logic for project and project tasks

--- src/app/db-sync/sync-controller.ts
+++ src/app/db-sync/sync-controller.ts
@@ -1,17 +1,17 @@
-import { ISFResponse, SFResponse } from './sf-response';
-import { SyncDataModel } from './sync.datamodel';
-import { IProjectDetails } from './../project/project.model';
-import { ISseResponse, sse } from '@toverux/expresse';
-import { OrgMasterModel } from '../org-master/org-master.model';
-import { SubService } from './sub-service';
-import * as express from 'express';
-import * as request from 'request';
-import * as rp from 'request-promise';
-import { Enums } from '../../config/enums';
-import { formatProjectAndTaskDetails } from '../project/project.helper';
-import { Constants } from '../../config/constants';
-import { EventEmitter } from 'events';
-import { IOrgMaster } from '../../core/models/org-master';
+import { ISFResponse, SFResponse } from "./sf-response";
+import { SyncDataModel } from "./sync.datamodel";
+import { IProjectDetails } from "./../project/project.model";
+import { ISseResponse, sse } from "@toverux/expresse";
+import { OrgMasterModel } from "../org-master/org-master.model";
+import { SubService } from "./sub-service";
+import * as express from "express";
+import * as request from "request";
+import * as rp from "request-promise";
+import { Enums } from "../../config/enums";
+import { formatProjectAndTaskDetails } from "../project/project.helper";
+import { Constants } from "../../config/constants";
+import { EventEmitter } from "events";
+import { IOrgMaster } from "../../core/models/org-master";
 
 /**
  * @description It gets all orgs and its integration user OAuth Token to be used for the subscription.
@@ -25,18 +25,25 @@ export class SyncController {
     this.subscribers = new Map();
     this.orgMasterModel = new OrgMasterModel();
     this.syncDataModel = new SyncDataModel();
-    this.eventBus.addListener('orgSynched', (vanityId: string) => {
-      console.log('Subscribing to the new org', vanityId);
+    this.eventBus.addListener("orgSynched", (vanityId: string) => {
+      console.log("Subscribing to the new org", vanityId);
       this.addNewOrgToSubscribers(vanityId);
     });
   }
   /**
    * events
    */
-  public events(req: express.Request, res: ISseResponse, next: express.NextFunction) {
-    this.eventBus.addListener('sendEvent', (args: { event: string, data: any }) => {
-      res.sse.event(args.event, args.data);
-    });
+  public events(
+    req: express.Request,
+    res: ISseResponse,
+    next: express.NextFunction
+  ) {
+    this.eventBus.addListener(
+      "sendEvent",
+      (args: { event: string; data: any }) => {
+        res.sse.event(args.event, args.data);
+      }
+    );
   }
   /**
    * @description Initializes the SubService instances for all the registered orgs.
@@ -46,9 +53,8 @@ export class SyncController {
       if (error1) {
         return;
       }
-      results.forEach((org) => {
+      results.forEach(org => {
         this.subscribers.set(org.org_id, new SubService(org));
-
       });
     });
   }
@@ -75,7 +81,11 @@ export class SyncController {
    * syncInitial
    * TODO: Make use of server events to perform the next action of synching.
    */
-  public syncInitial(req: express.Request, res: express.Response, next: express.NextFunction) {
+  public syncInitial(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
     const accessToken = req.body.accessToken;
     const vanityKey = req.params.vanityKey;
     const appUrl = req.body.appUrl;
@@ -85,118 +95,206 @@ export class SyncController {
         // Get project details from salesforce api
         const requestHeader = {
           headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json',
-          },
+            Authorization: "Bearer " + accessToken,
+            "Content-Type": "application/json"
+          }
         };
-        this.orgMasterModel.getOrgConfigByVanityId(vanityKey, (error, config) => {
-          if (!error && config) {
+        this.orgMasterModel.getOrgConfigByVanityId(
+          vanityKey,
+          (error, config) => {
+            if (!error && config) {
+              let sfResponsePerRequest: ISFResponse<
+                IProjectDetails
+              > = new SFResponse<IProjectDetails>();
 
-            let sfResponsePerRequest: ISFResponse<IProjectDetails> = new SFResponse<IProjectDetails>();
+              const url =
+                sfResponsePerRequest.nextRecordsUrl ||
+                config.api_base_url + Constants.SYNC_QUERIES.ALL;
+              const func = (
+                sfUrl: string,
+                callbackFunc: (formattedStuff: any) => void
+              ) => {
+                request(url, requestHeader, (error1, sfResponse) => {
+                  if (!error1) {
+                    if (sfResponse.statusCode === 401) {
+                      res.send({
+                        status: Enums.RESPONSE_STATUS.ERROR,
+                        message: sfResponse.body[0].message
+                      });
+                    } else {
+                      if (sfResponse.body && sfResponse.body.length > 0) {
+                        if (typeof sfResponse.body === "string") {
+                          sfResponsePerRequest = JSON.parse(
+                            sfResponse.body
+                          ) as ISFResponse<IProjectDetails>;
+                        } else {
+                          sfResponsePerRequest = JSON.parse(sfResponse.body);
+                        }
 
-            const url = sfResponsePerRequest.nextRecordsUrl || config.api_base_url + Constants.SYNC_QUERIES.ALL;
-            const func = (sfUrl: string, callbackFunc: (formattedStuff: any) => void) => {
-              request(url, requestHeader, (error1, sfResponse) => {
-                if (!error1) {
-                  if (sfResponse.statusCode === 401) {
-                    res.send({ status: Enums.RESPONSE_STATUS.ERROR, message: sfResponse.body[0].message });
-                  } else {
-                    if (sfResponse.body && sfResponse.body.length > 0) {
-                      if (typeof sfResponse.body === 'string') {
-                        sfResponsePerRequest = JSON.parse(sfResponse.body) as ISFResponse<IProjectDetails>;
-                      } else {
-                        sfResponsePerRequest = JSON.parse(sfResponse.body);
+                        const formatedProjects = formatProjectAndTaskDetails(
+                          sfResponsePerRequest.records
+                        );
+
+                        // res.send({ status: Enums.RESPONSE_STATUS.SUCCESS, message: '', projects: formatedProjects });
                       }
-
-                      const formatedProjects = formatProjectAndTaskDetails(sfResponsePerRequest.records);
-
-                      // res.send({ status: Enums.RESPONSE_STATUS.SUCCESS, message: '', projects: formatedProjects });
+                    }
+                  } else {
+                    res.render("error", {
+                      status: Enums.RESPONSE_STATUS.ERROR,
+                      message: error1
+                    });
+                    // res.send({ status: Enums.RESPONSE_STATUS.ERROR, message: error1 });
+                  }
+                });
+              };
+              func(url, (formatedProjects: any) => {
+                formatedProjects.map((self: any) => {
+                  self["OrgMaster_Ref_Id"] = config.vanity_id;
+                });
+                this.syncDataModel.syncSalesforceUserDetails(
+                  { vanity_id: vanityKey, session_id: accessToken },
+                  formatedProjects,
+                  done => {
+                    if (done && sfResponsePerRequest.done) {
+                      this.eventBus.emit("orgSynched", vanityKey);
+                      res.render("data-sync", { appUrl });
+                      // resolve(vanityKey);
+                      //     res.render('data-sync', { appUrl }, (err, html) => {
+                      //     console.log(err, html);
+                      //     this.eventBus.emit('sendEvent', { event: 'initialSyncDone', data: { appUrl } });
+                      //     return;
+                      // });
+                      return;
+                    } else if (done && !sfResponsePerRequest.done) {
+                      func(
+                        sfResponsePerRequest.nextRecordsUrl!,
+                        (formattedStuff: any) => {
+                          return;
+                        }
+                      );
+                      // res.render('data-sync', { appUrl });
+                      //     res.render('data-sync', { appUrl }, (err, html) => {
+                      //     console.log(err, html);
+                      //     this.eventBus.emit('sendEvent', { event: 'initialSyncDone', data: { error: 'There was a problem synching your data.' } });
+                      //     return;
+                      // });
+                    } else {
+                      res.render("data-sync", { appUrl });
+                      return;
+                      // reject('sync failed');
                     }
                   }
-                } else {
-                  res.render('error', { status: Enums.RESPONSE_STATUS.ERROR, message: error1 });
-                  // res.send({ status: Enums.RESPONSE_STATUS.ERROR, message: error1 });
-                }
+                );
               });
-            };
-            func(url, (formatedProjects: any) => {
-              formatedProjects.map((self: any) => {
-                self['OrgMaster_Ref_Id'] = config.vanity_id;
+              // const fetchAndSyncDataPromise = new Promise((resolve, reject) => vanityKey1);
+              // fetchAndSyncDataPromise.then((vanityKey1) => {
+              //   console.log(vanityKey1);
+              // }).catch((err) => {
+              //   console.log(err);
+              // });
+            } else {
+              res.render("error", {
+                status: Enums.RESPONSE_STATUS.ERROR,
+                message: error
               });
-              this.syncDataModel.syncSalesforceUserDetails(
-                { vanity_id: vanityKey, session_id: accessToken }, formatedProjects, (done) => {
-                  if (done && sfResponsePerRequest.done) {
-                    this.eventBus.emit('orgSynched', vanityKey);
-                    res.render('data-sync', { appUrl });
-                    // resolve(vanityKey);
-                    //     res.render('data-sync', { appUrl }, (err, html) => {
-                    //     console.log(err, html);
-                    //     this.eventBus.emit('sendEvent', { event: 'initialSyncDone', data: { appUrl } });
-                    //     return;
-                    // });
-                    return;
-                  } else if (done && !sfResponsePerRequest.done) {
-                    func(sfResponsePerRequest.nextRecordsUrl!, (formattedStuff: any) => {
-                      return;
-                    });
-                    // res.render('data-sync', { appUrl });
-                    //     res.render('data-sync', { appUrl }, (err, html) => {
-                    //     console.log(err, html);
-                    //     this.eventBus.emit('sendEvent', { event: 'initialSyncDone', data: { error: 'There was a problem synching your data.' } });
-                    //     return;
-                    // });
-                  } else {
-                    res.render('data-sync', { appUrl });
-                    return;
-                    // reject('sync failed');
-                  }
-
-                });
-            });
-            // const fetchAndSyncDataPromise = new Promise((resolve, reject) => vanityKey1);
-            // fetchAndSyncDataPromise.then((vanityKey1) => {
-            //   console.log(vanityKey1);
-            // }).catch((err) => {
-            //   console.log(err);
-            // });
-          } else {
-            res.render('error', { status: Enums.RESPONSE_STATUS.ERROR, message: error });
+            }
           }
-        });
-
+        );
       } else {
-        res.render('error', { status: Enums.RESPONSE_STATUS.ERROR, message: Constants.MESSAGES.INVALID_REQUEST_PARAMS });
+        res.render("error", {
+          status: Enums.RESPONSE_STATUS.ERROR,
+          message: Constants.MESSAGES.INVALID_REQUEST_PARAMS
+        });
         // res.send({ status: Enums.RESPONSE_STATUS.ERROR, message: Constants.MESSAGES.INVALID_REQUEST_PARAMS });
       }
     } catch (e) {
       console.log(e);
-      res.render('error', { status: Enums.RESPONSE_STATUS.ERROR, message: Constants.MESSAGES.SOMETHING_WENT_WRONG });
+      res.render("error", {
+        status: Enums.RESPONSE_STATUS.ERROR,
+        message: Constants.MESSAGES.SOMETHING_WENT_WRONG
+      });
       // res.send({ status: Enums.RESPONSE_STATUS.ERROR, message: Constants.MESSAGES.SOMETHING_WENT_WRONG });
     }
   }
   /**
-   * syncFirst
+   * getAllProjectsAndTasks
    */
-  public async syncFirst(req: express.Request, res: express.Response, next: express.NextFunction) {
+  private async getAllProjectsAndTasks(
+    vanityKey: string,
+    accessToken: string
+  ): Promise<ISFResponse<IProjectDetails>> {
+    try {
+      const config = await this.orgMasterModel.getOrgConfigByVanityId(
+        vanityKey
+      );
+      let sfResponsePerRequest: ISFResponse<IProjectDetails> = new SFResponse<
+        IProjectDetails
+      >();
+      const url =
+        sfResponsePerRequest.nextRecordsUrl ||
+        config.api_base_url + Constants.SYNC_QUERIES.ALL;
+      sfResponsePerRequest = await this.getData<IProjectDetails>(
+        url,
+        accessToken
+      );
+      for (const element of sfResponsePerRequest.records) {
+        element.Project_Task__c = await this.getData(
+          element.Project_Task__c.nextRecordsUrl!,
+          accessToken,
+          element.Project_Task__c.records
+        );
+      }
+      return sfResponsePerRequest;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async syncDataInitial(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
     const accessToken = req.body.accessToken;
     const vanityKey = req.params.vanityKey;
     const appUrl = req.body.appUrl;
-    try {
-      const config = await this.orgMasterModel.getOrgConfigByVanityId(vanityKey);
-      let sfResponsePerRequest: ISFResponse<IProjectDetails> = new SFResponse<IProjectDetails>();
-      const url = sfResponsePerRequest.nextRecordsUrl || config.api_base_url + Constants.SYNC_QUERIES.ALL;
-      sfResponsePerRequest = await this.getData<IProjectDetails>(url, accessToken);
-    } catch (error) {
+    const formatedProjects = await this.getAllProjectsAndTasks(
+      vanityKey,
+      accessToken
+    );
+    this.syncDataModel.syncSalesforceUserDetails(
+      { vanity_id: vanityKey, session_id: accessToken },
+      formatedProjects.records,
+      done => {
+        if (done) {
+          this.eventBus.emit("orgSynched", vanityKey);
+          res.render("data-sync", { appUrl });
+          return;
+        } else {
+          res.render("data-sync", { appUrl });
+          return;
+          // reject('sync failed');
 
-    }
-
+          // res.render('data-sync', { appUrl });
+          //     res.render('data-sync', { appUrl }, (err, html) => {
+          //     console.log(err, html);
+          //     this.eventBus.emit('sendEvent', { event: 'initialSyncDone', data: { error: 'There was a problem synching your data.' } });
+          //     return;
+          // });
+        }
+      }
+    );
   }
-  private async getData<T>(url: string, accessToken: string, records?: T[]): Promise<ISFResponse<T>> {
+  private async getData<T>(
+    url: string,
+    accessToken: string,
+    records?: T[]
+  ): Promise<ISFResponse<T>> {
     const requestHeader = {
       headers: {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json',
-      },
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json"
+      }
     };
     try {
       let partialData = new SFResponse<T>();
@@ -205,7 +303,7 @@ export class SyncController {
         throw response.body;
       } else {
         if (response.body && response.body.length > 0) {
-          if (typeof response.body === 'string') {
+          if (typeof response.body === "string") {
             partialData = JSON.parse(response.body) as ISFResponse<T>;
           } else {
             partialData = JSON.parse(response.body);
@@ -230,7 +328,9 @@ export class SyncController {
   /**
    * syncInitial
    */
-  public index(request: express.Request, response: express.Response, next: express.NextFunction) {
-
-  }
+  public index(
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) {}
 }
